2bc7f77b12566ee9ef579cd57b917f49d56e5d83
cmd430 <cmd430@gmail.com>
2022-08-05 00:12:44 +1200

    inital commit

---
+++ NoRoundCorners11/BackgroundApp.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BackgroundApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(BackgroundApp))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.StartWithWindowsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NoRoundCorners11"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartWithWindowsToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(177, 54)
+        '
+        'StartWithWindowsToolStripMenuItem
+        '
+        Me.StartWithWindowsToolStripMenuItem.Name = "StartWithWindowsToolStripMenuItem"
+        Me.StartWithWindowsToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.StartWithWindowsToolStripMenuItem.Text = "Start with Windows"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(173, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'BackgroundApp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(0, 0)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "BackgroundApp"
+        Me.Opacity = 0R
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.SystemColors.Control
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents StartWithWindowsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ NoRoundCorners11/BackgroundApp.vb
@@ -0,0 +1,108 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+
+Public Class BackgroundApp
+
+    <DllImport("dwmapi.dll", PreserveSig:=True)>
+    Private Shared Function DwmSetWindowAttribute(hwnd As IntPtr, attr As DwmWindowAttribute, ByRef attrValue As Integer, attrSize As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto, SetLastError:=True, EntryPoint:="SetWinEventHook", CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function SetWinEventHook(eventMin As UInteger, eventMax As UInteger, hmodWinEventProc As IntPtr, lpfnWinEventProc As WinEventDelegate, idProcess As UInteger, idThread As UInteger, dwFlags As UInteger) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto, SetLastError:=True, EntryPoint:="UnhookWinEvent", CallingConvention:=CallingConvention.StdCall)>
+    Private Shared Function UnhookWinEvent(hWinEventHook As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    Public Delegate Sub WinEventDelegate(hWinEventHook As IntPtr, eventType As UInteger, hWnd As IntPtr, idObject As Integer, idChild As Integer, dwEventThread As UInteger, dwmsEventTime As UInteger)
+
+    Enum DwmWindowAttribute As UInteger
+        NCRenderingEnabled = 1
+        NCRenderingPolicy
+        TransitionsForceDisabled
+        AllowNCPaint
+        CaptionButtonBounds
+        NonClientRtlLayout
+        ForceIconicRepresentation
+        Flip3DPolicy
+        ExtendedFrameBounds
+        HasIconicBitmap
+        DisallowPeek
+        ExcludedFromPeek
+        Cloak
+        Cloaked
+        FreezeRepresentation
+        PassiveUpdateMode
+        UseHostBackdropBrush
+        UseImmersiveDarkMode = 20
+        WindowCornerPreference = 33
+        BorderColor
+        CaptionColor
+        TextColor
+        VisibleFrameBorderThickness
+        SystemBackdropType
+        Last
+    End Enum
+
+    Enum DwmWindowCornerPreference As Long
+        DoNotRound = 1
+        Round = 2
+        SemiRound = 3
+    End Enum
+
+    Private Const WINEVENT_OUTOFCONTEXT As UInteger = 0
+    Private Const EVENT_SYSTEM_FOREGROUND As UInteger = 3
+
+    Private ReadOnly WinEventProcDelegate As New WinEventDelegate(AddressOf WinEventProc)
+    Private ReadOnly WinEventHook As IntPtr = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, WinEventProcDelegate, 0, 0, WINEVENT_OUTOFCONTEXT)
+    Private ReadOnly TaskSchedeuler As New Scheduler("NoRoundCorners11", "cmd430", "Starts NoRoundCorners11 on Logon", Nothing, Nothing, False)
+
+    Private Sub BackgroundApp_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If TaskSchedeuler.GetTask() Is Nothing Then
+            TaskSchedeuler.AddTask()
+        Else
+            TaskSchedeuler.UpdateTask()
+        End If
+        If LoadWithWindows() Then
+            StartWithWindowsToolStripMenuItem.Checked = True
+        End If
+        AddHandler StartWithWindowsToolStripMenuItem.CheckedChanged, AddressOf StartWithWindowsToolStripMenuItem_CheckedChanged
+    End Sub
+
+    Public Function LoadWithWindows() As Boolean
+        Return TaskSchedeuler.GetTask().Enabled
+    End Function
+
+    Private Sub WinEventProc(hWinEventHook As IntPtr, eventType As UInteger, HWND As IntPtr, idObject As Integer, idChild As Integer, dwEventThread As UInteger, dwmsEventTime As UInteger)
+        DwmSetWindowAttribute(HWND, DwmWindowAttribute.WindowCornerPreference, DwmWindowCornerPreference.DoNotRound, Marshal.SizeOf(GetType(Integer)))
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If Not LoadWithWindows() Then
+            TaskSchedeuler.DeleteTask()
+        End If
+        UnhookWinEvent(WinEventHook)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub StartWithWindowsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartWithWindowsToolStripMenuItem.Click
+        StartWithWindowsToolStripMenuItem.Checked = False
+    End Sub
+
+    Private Sub StartWithWindowsToolStripMenuItem_CheckedChanged(sender As Object, e As EventArgs)
+        RemoveHandler StartWithWindowsToolStripMenuItem.CheckedChanged, AddressOf StartWithWindowsToolStripMenuItem_CheckedChanged
+        If LoadWithWindows() Then
+            TaskSchedeuler.ToggleTask()
+            StartWithWindowsToolStripMenuItem.Checked = False
+        Else
+            TaskSchedeuler.ToggleTask()
+            StartWithWindowsToolStripMenuItem.Checked = True
+        End If
+        AddHandler StartWithWindowsToolStripMenuItem.CheckedChanged, AddressOf StartWithWindowsToolStripMenuItem_CheckedChanged
+    End Sub
+
+End Class
